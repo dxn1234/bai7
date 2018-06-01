6053ee15dbe4986f2656439b20db6ef4cad96317
nguyenduc1997 <ducnguyenptit1909@gmail.com>
2018-05-31 21:35:10 -0700

    bai7

---
+++ app/src/main/java/com/example/administrator/demojsonobject/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.administrator.demojsonobject;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public class ReadJson extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+}
